869c76034adfa2514d1a5d8195e5be576c8de365
MohamedMaatougui <Mohamed.Maatougui@esprit.tn>
2025-12-09 15:33:33 +0100

    Clean repo: remove wrong files

--- ConferenceApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Conference
-
-class ConferenceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Conference
-        fields = '__all__'
-        #read_only_fields = ['conference_id','created_at','update_at']
